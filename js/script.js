59952ad7ef2a3f1fa1f3d54185b2651337031120
Christopher Lopez <chrisa_lopez@yahoo.com>
2016-06-28 12:02:24 -0700

    refactor square function

--- js/script.js
+++ js/script.js
@@ -8,16 +8,34 @@ var squares = document.querySelectorAll('.square')
 // set up click listeners for my squares
 for (var i = 0; i < 9 ; i++) {
     squares[i].addEventListener('click', function (e) {
-      // loop through squares again and check with node was clicked
-      for (var j=0; j < 9; j++) {
-        if (squares[j] === e.target && board[j] === '') {
-          board[j] = turn
+      var squareIndex = getSquareIndex(e.target)
+      if (board[squareIndex] === '') {
+          board[squareIndex] = turn
           drawBoard()
           switchTurn()
-        }
       }
-  })
+    })
+  }
+      // loop through squares again and check with node was clicked
+//       for (var j=0; j < 9; j++) {
+//         if (squares[j] === e.target && board[j] === '') {
+//           board[j] = turn
+//           drawBoard()
+//           switchTurn()
+//         }
+//       }
+//   })
+// }
+// given an HTML element, it will determine the index of the square of the board
+function getSquareIndex(target) {
+  for (var j=0; j < 9; j++) {
+    // check if the current iterated square is the square that was clicked
+    if (squares[j] === target) {
+      return j; //return the index, not the DOM
+    }
+  }
 }
+
 // drawing the model to the screen
 function drawBoard () {
   for (var i = 0; i < 9 ; i++) {
